84e2f69cd67c1164f8a139c97410e8e814c46d10
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-20 22:33:32 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,37 @@
+const express  = require('express')
+const path = require("path");
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const cors = require("cors")
+config()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use(cors())
+app.use("/", (req, res) => {
+    res.render('index.html')
+})
+
+let dataBase = []
+
+io.on('connection', socket => {
+    socket.emit("synchronize", dataBase)
+    socket.broadcast.emit("synchronize", dataBase)
+    socket.on("login", data => {
+        dataBase.push(data)
+        socket.emit("synchronize", dataBase)
+        socket.broadcast.emit("synchronize", dataBase)
+    })
+    socket.on("update", data => {
+        dataBase = data;
+        socket.emit("synchronize", dataBase)
+        socket.broadcast.emit("synchronize", dataBase)
+    })
+})
+
+server.listen(3000)
+
+
