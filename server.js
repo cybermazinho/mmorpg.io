e33cfb997e812e17498d7e30402d93f9f4e47b84
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-24 16:05:59 -0500

    fix: corrigindo implementação de chat

--- server.js
+++ server.js
@@ -16,16 +16,27 @@ app.use("/", (req, res) => {
     res.render('index.html')
 })
 
+let messages = []
+
 
 io.on('connection', socket => {
+    
     socket.emit("map_player")
     socket.broadcast.emit("map_player")
+    socket.emit('messages', messages)
    
     socket.on("created_player", data => {
         socket.broadcast.emit("login", data)
     })
     socket.on('move_player', data => {
         socket.broadcast.emit("synchronize", data)
+    })
+
+    socket.on('message', data => {
+        messages.length >=15 ? messages = [] : messages
+        messages.push(data)
+        socket.emit('messages', messages)
+        socket.broadcast.emit('messages', messages)
     })
     
 })
